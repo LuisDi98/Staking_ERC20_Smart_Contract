1ac9c6091d79056ff7e616612a2926b5e3ad45dd
Luis Diego Mora Aguilar <39139078+LuisDi98@users.noreply.github.com>
2024-09-22 23:14:57 -0600

    tweaks

--- Staking_ERC20.sol
+++ Staking_ERC20.sol
@@ -24,7 +24,7 @@ contract MyToken is ERC20 {
     constructor(uint _basisPoints)ERC20("THREE", "THR"){ 
         basisPoints = _basisPoints;
         _mint(address(this), 1000000000000000000);
-        _mint(0x7dC329c73a29c2E6606670c9F03B38976DE4E328, 1000);
+        _mint(msg.sender, 1000);
         token = IERC20(address(this));
     }
 
