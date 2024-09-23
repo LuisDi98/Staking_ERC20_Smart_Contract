314e1e0e4edb577371ac4c3ea29cd54ae2546790
Luis Diego Mora Aguilar <39139078+LuisDi98@users.noreply.github.com>
2024-09-22 23:06:48 -0600

    Contract fixed + Staking tested

--- Staking_ERC20.sol
+++ Staking_ERC20.sol
@@ -1,58 +1,44 @@
+//https://www.youtube.com/watch?v=1IDqOXRF-Ls
+//https://www.youtube.com/watch?v=lQtf6mI1D70&t=160s
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/*
-* @notice Hace staking de tokens
-* @dev Permite al owner definir el porcentaje de staking con base
-* en basis points
-*/
-contract StakingContract is ERC20 {
+contract MyToken is ERC20 {
 
   struct Staker {
         uint stakedTokens;
         uint lastStakeMovement;
     }
 
-    //mapping que relaciona la direccion del usuario con
-    //los tokens bloqueadas y la fecha y hora de la ultima transaccion
     mapping(address => Staker) public balances;
-    
     uint public basisPoints;
+    IERC20 public token;
 
     event Stake(address _staker, uint _stakedTokens);
     event Unstake(address _staker, uint _unstakedTokens);
+    event Print(string _message, uint256 _reward);
 
     constructor(uint _basisPoints)ERC20("THREE", "THR"){ 
         basisPoints = _basisPoints;
-        _mint(msg.sender, 1 * 10 ** 18);
+        _mint(address(this), 1000000000000000000);
+        _mint(0x7dC329c73a29c2E6606670c9F03B38976DE4E328, 1000);
+        token = IERC20(address(this));
     }
 
-    /*
-    * @notice Bloquea tokens para hacer staking
-    * @param _numTokens Numero de tokens que seran bloqueados para el staking
-    */
     function stake(uint256 _numTokens) external {
         require(balanceOf(msg.sender) >= _numTokens, "Not enough balance");
 
         _transfer(msg.sender, address(this), _numTokens);
 
-        //registra la cantidad de tokens bloqueados
         balances[msg.sender].stakedTokens += _numTokens;
-        
-        //registra la fecha y hora en que se bloquean los toques, en segundos
         balances[msg.sender].lastStakeMovement = block.timestamp;
 
         emit Stake(msg.sender, _numTokens);
     }
 
-    /*
-    * @notice Desbloque tokens
-    * @dev Devuelve los toques bloqueados en el contrato hacia el 
-    * usuario
-    * @param _numTokens Numero de tokens que serán desbloqueados y devueltos
-    */
     function unstake(uint256 _numTokens)external{
         require(balances[msg.sender].stakedTokens >= _numTokens, "Insufficient staked tokens");
         balances[msg.sender].stakedTokens -= _numTokens;
@@ -61,44 +47,27 @@ contract StakingContract is ERC20 {
         emit Unstake(msg.sender, _numTokens);
     }
 
-    /*
-    * @notice Calcula la recompensa por los tokens en staking
-    * @param _staker Direccion del usuario
-    * @returns Numero de tokens ganados por el staking
-    */
     function calculateReward(address _staker) public view returns (uint256){
         Staker storage staker = balances[_staker];
 
-        //diferencia de tiempo en segundos entre el tiempo de solicitar
-        //la recompensa y el momento de staking mas reciente
-        uint256 timeDiff = block.timestamp - staker.lastStakeMovement; 
-        
-        //apr basada en basisPoints
-        uint256 apr = calculateAPR();
-
-        uint256 stakingReward = (staker.stakedTokens * apr * timeDiff) / (365 days * 1e18);
+        uint256 timeDiff = block.timestamp - staker.lastStakeMovement; //diferencia de tiempo en s
+        uint256 apr = calculateAPR();//apr basada en basisPoints
+        uint256 stakingReward = (staker.stakedTokens * apr * timeDiff) / (86400 * 1e18);//*
 
         return stakingReward;
     }
 
-    /*
-    * @notice Calcula el Annual Percentage Rate 
-    * @dev Se calcula basos en basis points
-    * @returns Annual Percentage Rate 
-    */
     function calculateAPR()internal view returns(uint256){
         return basisPoints * 1e18 / 10000; 
     }
 
-    /*
-    * @notice Reclama la ganancia obtenida con el staking
-    * @dev A partir de la recompensa se crea (mint) tokens para poder transferiral usuario
-    */
-    function claimReward()external payable returns (bool){
+    function claimReward()external returns (bool){
         uint256 reward = calculateReward(msg.sender);
         require(reward > 0, "No rewards available");
         balances[msg.sender].lastStakeMovement = block.timestamp;
-        _mint(msg.sender, reward);
+        require(token.transfer(msg.sender, reward), "Transaction Failed.");
         return true;
     }
 }
+// Main: 0x7dC329c73a29c2E6606670c9F03B38976DE4E328
+// Test: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
